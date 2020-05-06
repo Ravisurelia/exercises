14f8b2cc8c52b2a98a973f3fb626993e49ed7c91
root <root@M.localdomain>
2020-05-06 18:54:48 +0200

    other way of doing exercise2

---
+++ Wednesay_exercises/execrcise2_normalmethod.js
@@ -0,0 +1,8 @@
+function waitThenRun(delay){
+    setTimeout(delay, 1500)
+}
+    function greeting(){
+        console.log('Hello! and Goodbye!can not wait this long.');
+    }
+
+waitThenRun(greeting);
