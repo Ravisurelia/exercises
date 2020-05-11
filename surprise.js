68ceef09ea73f7839a477e3d797d5bcc9979176d
root <root@M.localdomain>
2020-05-11 13:55:19 +0200

    sureprise exercise

---
+++ surprise.js
@@ -0,0 +1,20 @@
+var x = 100;
+var doubleX = 200;
+
+function timesTwo(num) {
+    return num * 2;
+    
+}
+console.log((timesTwo(x)));
+
+
+var numbers = [100, 200];
+for (var = i; i > Array.length; i++);
+console.log(numbers[0]);
+console.log(numbers[1]);
+
+function name() {
+    y : doubleX 
+}
+
+
