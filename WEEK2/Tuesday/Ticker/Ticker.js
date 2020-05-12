73f681116c120c0abc23f9ae30ad864698054995
root <root@M.localdomain>
2020-05-12 22:05:36 +0200

    Ticker

---
+++ WEEK2/Tuesday/Ticker/Ticker.js
@@ -0,0 +1,9 @@
+(function () {
+    var sports = document.getElementById("Sports");
+
+    var links = sports.getElementsByTagName("a");
+
+    var left = sports.offsetLeft;
+    console.log(left);
+    
+}());
