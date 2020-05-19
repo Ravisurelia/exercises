a6fbbcc83fafe93e92bd2bd26ea4aaf94f54966a
root <root@M.localdomain>
2020-05-19 23:18:01 +0200

    pushing_ticker

--- WEEK3/Tuesday/jqueary/exercise/ticket_redefine/Ticker.js
+++ WEEK3/Tuesday/jqueary/exercise/ticket_redefine/Ticker.js
@@ -1,38 +1,59 @@
 (function () {
-    var request;
+    var qRequest;
 
-    var sports = document.getElementById("Sports");
-    var links = sports.getElementsByTagName("a");
-    var linksArray = [];
+    //var sports = document.getElementById("Sports");
+    var qSports = $("#Sports");
+    //var links = sports.getElementsByTagName("a");
+    var qLinks = $("a");
+    //var linksArray = [];
+    var qLinksArray = [];
 
-    for (var i = 0; i < links.length; i++) {
-        linksArray.push(links[i]);
+    for (var i = 0; i < qLinks.length; i++) {
+        qLinksArray.push(qLinks[i]);
     }
 
-    for (var k = 0; k < links.length; k++) {
-        linksArray[i].addEventListener("mouseover", cancelSlide);
-        linksArray[i].addEventListener("mouseout", resumeSlide);
-        console.log(linksArray[i]);
+    for (var k = 0; k < qLinks.length; k++) {
+        qLinksArray[i].addEventListener("mouseover", cancelSlide);
+        qLinksArray[i].addEventListener("mouseout", resumeSlide);
+        console.log(qLinksArray[i]);
     }
+    
     slide();
     function slide() {
-        var left = sports.offsetLeft;
-        left--;
-        sports.style.left = left + "px";
-        if (left <= -links[0].offsetWidth) {
-            sports.style.left = left + links[0].offsetWidth + "px";
-            links[0].parentElement.appendChild(links[0]);
+        //var left = sports.offsetLeft;
+        var qLeft = qSports.offset().Left;
+        qLeft--;
+        //sports.style.left = left + "px";
+        qSports.css({
+            left: qSports + "px",
+        });
+
+        if (qLeft <= -qLinks[0].outerwidth()) {
+            qSports.style.qLeft = qLeft + qLinks[0].outerwidth + "px";
+            qLinks[0].parent.append(qLinks[0]);
         }
-        request = requestAnimationFrame(slide);
+        qRequest = requestAnimationFrame(slide);
     }
     function cancelSlide (event) {
-        event.target.style.color = "yellow";
-        event.target.style.textDecoration = "underline";
-        cancelAnimationFrame(request);
+        //event.target.style.color = "yellow";
+        event.target.css({
+            color: "yellow",
+        });
+        //event.target.style.textDecoration = "underline";
+        event.target.css({
+            "text-decoration": "underline",
+        });
+        cancelAnimationFrame(qRequest);
     }
     function resumeSlide (event) {
-        event.target.style.color = "white";
-        event.target.style.textDecoration = "";
+        //event.target.style.color = "white";
+        event.target.css({
+            color: "white",
+        });
+        //event.target.style.textDecoration = "";
+        event.target.css({
+            "text-decoration": "",
+        });
         requestAnimationFrame(slide);
     } 
 }());
