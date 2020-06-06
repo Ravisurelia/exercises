e36021b51a7188b58d6520b0f1a79b8459b01bef
root <root@M.localdomain>
2020-06-06 20:04:04 +0200

    portfolio

--- WEEK5/portfolio/index.js
+++ WEEK5/portfolio/index.js
@@ -1,26 +1,24 @@
 const http = require("http");
 const fs = require("fs");
 const path = require("path");
+const { myHtml } = require("./module.js");
+//console.log(addHtml);
 
 const server = http.createServer(function (req, res) {
-  //this is how we stream------------
-  console.log("__dirname: ", __dirname);
-  const readStream = fs.createReadStream(
-    __dirname + "/projects/spotify_search_2/style.css"
-  );
-  readStream.pipe(res);
+  //console.log("__dirname: ", __dirname);
 
   if (req.method !== "GET") {
     res.statusCode = 405; //method not allowed
     return res.end();
   }
+  if (req.url === "/") {
+    return res.end(myHtml());
+  }
 
   const filePath = __dirname + "/projects" + req.url;
-  console.log("filePath: ", filePath);
+  //console.log("filePath: ", filePath);
 
   //lets deal with a potential attack
-  /*  console.log("/user/petea/../../../wp-config/passwords.txt");
-  console.log(path.normalize("/user/petea/../../../wp-config/passwords.txt")); */
 
   if (!path.normalize(filePath).startsWith(__dirname + "/projects")) {
     res.statusCode = 403;
@@ -28,35 +26,53 @@ const server = http.createServer(function (req, res) {
     return res.end();
   }
 
+  const obj = {
+    ".html": "text/html",
+    ".css": "text/css",
+    ".js": "text/javascript",
+    ".json": "application/json",
+    ".gif": "image/gif",
+    ".jpg": "image/jpeg",
+    ".png": "image/png",
+    ".svg": "image/svg+xml",
+  };
+  console.log(filePath);
+
   fs.stat(filePath, (err, stats) => {
     if (err) {
-      console.log("err in fs.stat; ", err);
+      console.log(("err in fs.stat; ", err));
       res.statusCode = 404;
       return res.end();
     }
-    console.log("lets do something");
+
     if (stats.isFile()) {
-      console.log("is s file");
+      //console.log("is s file");
       // we need to set the correct content type header
       //res.setHeader("Content-Type", "text/css");
-
-      //we can use the core noe module, path to hel us figure out the extension name
+      //we can use the core node module, path to hel us figure out the extension name
       const extension = path.extname(filePath);
-      console.log("extension: ", extension);
+      //console.log("extension: ", extension);
+      res.setHeader("content-type", obj[extension]);
+
+      const readStreamHtml = fs.createReadStream(filePath);
+      readStreamHtml.pipe(res);
     } else {
-      console.log("is a directory");
+      //console.log("is a directory");
       if (req.url.endsWith("/")) {
         const readStreamHtml = fs.createReadStream(filePath + "/index.html");
         readStreamHtml.pipe(res);
+
         readStreamHtml.on("error", (err) => {
           console.log(err);
           res.statusCode = 500;
           return res.end();
         });
       } else {
-        //we need to rediredt to the
+        req.setheader("Location", req.url + "/");
+        res.statusCode = 302;
+        res.end();
       }
     }
   });
 });
-server.listen(8081, () => console.log("portfolio is ready to run"));
+server.listen(8080, () => console.log("portfolio is ready to run"));
